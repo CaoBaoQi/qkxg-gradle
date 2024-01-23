e54995df5c35fb84d128c4b52d61bce076f6c723
曹宝琪 <2023cbq@gmail.com>
2024-01-23 22:30:43 +0800

    sb3-gradle: init commit

---
+++ sb3-gradle/src/main/java/jzxy/cbq/sb3gradle/Sb3GradleApplication.java
@@ -0,0 +1,13 @@
+package jzxy.cbq.sb3gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb3GradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb3GradleApplication.class, args);
+    }
+
+}
---
+++ sb3-gradle/src/test/java/jzxy/cbq/sb3gradle/Sb3GradleApplicationTests.java
@@ -0,0 +1,13 @@
+package jzxy.cbq.sb3gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb3GradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
