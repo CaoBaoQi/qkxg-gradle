812debe4fdfe709c1d3527718d65f29127497137
曹宝琪 <2023cbq@gmail.com>
2024-01-23 22:33:44 +0800

    sb3-gradle: go

--- sb3-gradle/src/main/java/jzxy/cbq/sb3gradle/Sb3GradleApplication.java
+++ sb3-gradle/src/main/java/jzxy/cbq/sb3gradle/Sb3GradleApplication.java
@@ -1,13 +1,17 @@
 package jzxy.cbq.sb3gradle;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class Sb3GradleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Sb3GradleApplication.class, args);
+
+        log.info("Sb3GradleApplication run successful ");
     }
 
 }
